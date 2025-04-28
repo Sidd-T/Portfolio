cad45977ba3030915d79ce6513a6657f5615209f
Siddhant Topiwala <sidd.topiwala02@gmail.com>
2025-04-28 15:53:00 -0400

    bugfix: add mobile optimizations for contact

--- src/components/Contact/ContactInfo.tsx
+++ src/components/Contact/ContactInfo.tsx
@@ -18,7 +18,7 @@ const ContactInfo = () => {
   }
 
   return (
-      <div className="xs:w-[90%] sm:w-[70%] md:w-[50%] lg:w-[40%] xl:w-[30%]">
+      <div className="w-full sm:w-[70%] md:w-[50%] lg:w-[40%] xl:w-[30%]">
         <div className="sticky-outer">
           <div className="sticky">
             <svg width="0" height="0">
@@ -33,7 +33,7 @@ const ContactInfo = () => {
               </defs>
             </svg>
             <div className="sticky-content cursor-pointer" onClick={CopyToClipboard}>
-              <p className={`mb-12 text-base !leading-relaxed text-dark dark:text-white sm:text-xl md:text-2xl ${caveat.className}`}>
+              <p className={`mb-12 text-base !leading-relaxed text-dark sm:text-xl md:text-2xl ${caveat.className}`}>
                 Email (click to copy): <br></br>
                 <span 
                   className="bg-gradient-to-r dark:from-[#00ff6a] dark:to-[#00d9ff] from-[#8300e7] to-[#0612ba] hero glitch layers" 
